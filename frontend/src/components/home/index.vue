fd36cc5202c6660fa9915b992c47599c8fcf29e2
Bishwo Bhandari <bhanbish@isu.edu>
2020-11-09 11:06:52 -0500

    new commit

--- frontend/src/components/home/index.vue
+++ frontend/src/components/home/index.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
         <Topbar></Topbar>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../../assets/logo.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="" alt="Third slide">
+    </div>
+  </div>
+</div>
         <ConstituentProperties/>
     </div>
 </template>
